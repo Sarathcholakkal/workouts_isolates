45a2d2f967da12f5e9ff982526b53d9c1ac25a60
sarath <ssarathcholakkall@gmail>
2024-11-19 13:33:05 +0530

    home and main

---
+++ lib/home.dart
@@ -0,0 +1,56 @@
+import 'dart:isolate';
+import 'package:flutter/material.dart';
+import 'package:flutter_isolates_demo/complextask.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset('assets/gifs/bouncing-ball.gif'),
+              //Blocking UI task
+              ElevatedButton(
+                onPressed: () async {
+                  var total = await complexTask1();
+                  debugPrint('Result 1: $total');
+                },
+                child: const Text('Task 1'),
+              ),
+              //Isolate
+              ElevatedButton(
+                onPressed: () async {
+                  final receivePort = ReceivePort();
+                  await Isolate.spawn(complexTask2, receivePort.sendPort);
+                  receivePort.listen((total) {
+                    debugPrint('Result 2: $total');
+                  });
+                },
+                child: const Text('Task 2'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Task 3'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<double> complexTask1() async {
+    var total = 0.0;
+    for (var i = 0; i < 1000000000; i++) {
+      total += i;
+    }
+    return total;
+  }
+}
+//--End of HomePage--
+
